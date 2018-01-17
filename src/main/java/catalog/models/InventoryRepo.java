a667113982512e3bdb7d53d4b4cf32483bb40f66
Bluemix Student <bmxstudent01@gmail.com>
2018-01-17 14:58:30 +0000

    Corrected Inventory code

--- src/main/java/catalog/models/InventoryRepo.java
+++ src/main/java/catalog/models/InventoryRepo.java
@@ -2,8 +2,14 @@ package catalog.models;
 
 import java.util.List;
 import javax.transaction.Transactional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+/**
+ * Orders Repository
+ *
+ */
 
 @Repository("itemsRepo")
 @Transactional
